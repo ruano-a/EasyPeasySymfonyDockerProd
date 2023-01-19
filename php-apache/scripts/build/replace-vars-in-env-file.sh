ebcdc2c928528ee65285504dda900245a5f69bfe
Aurélien Ruano <aurelien.ruano@epitech.eu>
2023-01-19 17:12:41 +0900

    fix var remplacement for mailer

--- php-apache/scripts/build/replace-vars-in-env-file.sh
+++ php-apache/scripts/build/replace-vars-in-env-file.sh
@@ -5,9 +5,9 @@ EMAIL_PASSWORD=`php -r "echo urlencode('$EMAIL_PASSWORD');"`
 I_WANT_MAILSERVER=`echo "$I_WANT_MAILSERVER" | tr '[:upper:]' '[:lower:]' `
 
 if [ "$I_WANT_MAILSERVER" = 'true' ];
-    then sed -i "s,^MAILER_URL=.*$,MAILER_URL=smtp://mailserver:25?username=${EMAIL}\&password=${EMAIL_PASSWORD}," "/var/www/html/$PROJECT_FOLDER_NAME/.env"
-fi
-
-if [ "$I_WANT_MAILSERVER" = 'true' ];
-    then sed -i "s,^MAILER_DSN=.*$,MAILER_DSN=smtp://mailserver:25?username=${EMAIL}\&password=${EMAIL_PASSWORD}," "/var/www/html/$PROJECT_FOLDER_NAME/.env" ;
+    then
+        # there is a \before & because sed does special things when it sees it
+        echo 'Replacing symfony .env mailer vars.'
+        sed -i "s,^MAILER_URL=.*$,MAILER_URL=smtp://mailserver:25?username=${EMAIL}\&password=${EMAIL_PASSWORD}," "/var/www/html/$PROJECT_FOLDER_NAME/.env"
+        sed -i "s,^MAILER_DSN=.*$,MAILER_DSN=smtp://mailserver:25?username=${EMAIL}\&password=${EMAIL_PASSWORD}," "/var/www/html/$PROJECT_FOLDER_NAME/.env"
 fi
